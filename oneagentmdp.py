83018630b3f554672ea92d583c97e6ffaf7568dd
linfengli222 <linfengli222@gmail.com>
2022-10-18 02:01:08 -0700

    Update oneagentmdp.py

--- oneagentmdp.py
+++ oneagentmdp.py
@@ -1,34 +1,36 @@
 import numpy as np
+from array import *
 # import pandas as pd
 
-from array import *
-
+#given current position(x, y), grid, action (stay=0, up=1, down=2, right=3, left=4), and p_e
+#output 2d array of probabilities of next state
+def stateEquation(x, y, grid, action, p_e):
+    actions = np.array([[0,0],[0,1],[0,-1],[1,0],[-1,0]])
+    probability = np.array([[0.0, 0.0, 0.0, 0.0, 0.0],
+                            [0.0, 0.0, 0.0, 0.0, 0.0],
+                            [0.0, 0.0, 0.0, 0.0, 0.0],
+                            [0.0, 0.0, 0.0, 0.0, 0.0],
+                            [0.0, 0.0, 0.0, 0.0, 0.0]])
+    for i in range(0,5):
+        if(i == action):
+            if((x+actions[i][0])<0 or (x+actions[i][0])>4 or (y+actions[i][1])<0 or (y+actions[i][1])>4):
+                probability[x][y]+=1-p_e
+            else:
+                probability[x+actions[i][0]][y+actions[i][1]] += 1-p_e
+        else:
+            if ((x+actions[i][0])<0 or (x+actions[i][0])>4 or (y+actions[i][1])<0 or (y+actions[i][1])>4):
+                probability[x][y] += p_e/4
+            else:
+                probability[x+actions[i][0]][y+actions[i][1]] += p_e/4
+    return probability
 
 #red=-1, green=1, white=0, grey=-2
-
-grid = np.array([[0, 0, 0, 0, -1],
+gridWorld = np.array([[0, 0, 0, 0, -1],
      [0, -2, -2, 0, -1],
      [0, 0, 1, 0, -1],
      [0, -2, -2, 0, -1],
      [0, 0, 1, 0, -1]])
-
-print(grid[1][1])
-x=0
-y=0
-st=grid[x][y]
-
-action=([0,0],[0,1],[0,-1],[1,0],[-1,0])#stay0,up1,down2,right3,left4
-print (action[0][0])#---0
-
-#input current state and action,
-#output 2d array of probabilities of next state
-def my_function(grid,action):
-    for a in range(0,4)
-     if x+action[a][0]<0 or x+action[a][0]>4 or y+action[a][1]<0 or y+action[a][1]>4
-         a=0 #stay
-     else stnext=grid[x+action[a][0]][y+action[a][1]]
-    
-
-
-
-my_function()
+actions = np.array([[0,0],[0,1],[0,-1],[-1,0],[0,1]])
+x=y=0
+                   
+print(stateEquation(1,1,gridWorld,2,0.1))
