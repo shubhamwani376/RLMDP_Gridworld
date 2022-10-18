3a0ba56da0547ab6ffc5bd87a339854c61eb1f64
linfengli222 <linfengli222@gmail.com>
2022-10-17 20:55:49 -0700

    Update oneagentmdp.py

--- oneagentmdp.py
+++ oneagentmdp.py
@@ -1,2 +1,17 @@
 import numpy as np
 # import pandas as pd
+
+from array import *
+import numpy as np
+
+#red=-1, green=1, white=0, grey=-2
+
+T = [[0, 0, 0, 0, -1],
+     [0, -2, -2, 0, -1],
+     [0, 0, 1, 0, -1],
+     [0, -2, -2, 0, -1],
+     [0, 0, 1, 0, -1]]
+
+print(T)
+
+
