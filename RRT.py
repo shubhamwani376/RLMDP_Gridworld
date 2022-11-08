8d695e1b80238349667a11f6e76ab34803a58c74
Shubham Wani <45815852+shubhamwani376@users.noreply.github.com>
2022-11-07 20:41:50 -0800

    added RRT.py

---
+++ RRT.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+
+if __name__ = "__main__"
+    start_pos = np.array([0.25,0.25,0]);
+    end_pos = np.array([0.75,0.75,np.pi]);
